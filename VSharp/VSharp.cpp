6097e4f86d80a3b93abd4f5f8540f9b4390f1b34
waifushork <waifushork@gmail.com>
2021-10-12 05:32:29 -0400

    I broke the repl but it compiles now

--- VSharp/VSharp.cpp
+++ VSharp/VSharp.cpp
@@ -1,9 +1,10 @@
 #include <System\Console.hpp>
 
 #include "System\Integer.hpp"
+#include "System\Float.hpp"
 
 // Main should always be kept as small as possible for simplistic sake
 System::Int32 main() noexcept
 {
-	System::Console::WriteLine("Hello world!");
+	System::Console::WriteLine("Hello world");
 }
